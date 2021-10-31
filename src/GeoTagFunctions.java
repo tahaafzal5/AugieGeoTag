0cd0c77eb93175fbe7a9e20fc04c68785785ed21
Taha Afzal <tahaafzal5@hotmail.com>
2021-10-31 16:35:29 -0500

    added function headers and assets

---
+++ src/GeoTagFunctions.java
@@ -0,0 +1,37 @@
+class GeoTagFunctions {
+    /* public ... boolean isJpeg(...) {
+        ...
+    } */
+
+    /* public ... ... readImageMeta(...) {
+        ...
+    } */
+
+    /* public ... boolean hasGeoTagData(...) {
+        ...
+    } */
+
+    /* public ... ... getGeoTagData(...) {
+        ...
+    } */
+
+    /* public ... void printGeoTagData(...) {
+        ...
+    } */
+
+    /* public ... boolean updateGeoTagData(...) {
+        ...
+    } */
+
+    /* public ... ... writeGeoTagData(...) {
+        ...
+    } */
+
+    /* public ... boolean removeGeoTagData(...) {
+        ...
+    } */
+
+    /* public ... boolean saveJpegImage(...) {
+        ...
+    } */
+}
---
+++ src/Utility.java
@@ -0,0 +1,30 @@
+public class Utility {
+
+    /* public ... void displayMenu(...) {
+        ...
+    } */
+
+    /* public ... ... handleUserMenuChoice(...) {
+        ...
+    } */
+
+    /* public ... void askConfirmation(...) {
+
+    } */
+
+    /* public ... ... handleUserConfirmationChoice(...) {
+        ...
+    } */
+
+    /* public ... void displayError(...) {
+        ...
+    } */
+
+    /* public ... void displaySuccess(...) {
+        ...
+    } */
+
+    /* public ... void displayProcessing(...) {
+        ...
+    } */
+}
