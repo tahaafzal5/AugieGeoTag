9d368611555ef3077ec3658c69278c658b1e3e01
Sicheng Yang <43109893+SichengYang@users.noreply.github.com>
2021-11-08 17:06:23 -0600

    Update GeoTagFunctions.java

--- src/GeoTagFunctions.java
+++ src/GeoTagFunctions.java
@@ -7,18 +7,18 @@ public class GeoTagFunctions {
   	public static boolean isJpeg(File f) throws IOException, FileNotFoundException
   	{    
   		if(!f.exists()) throw new FileNotFoundException("File does not exists");
-          BufferedInputStream buff = new BufferedInputStream(new FileInputStream(f));
+        BufferedInputStream buff = new BufferedInputStream(new FileInputStream(f));
               
-          //read the marker to make sure it is a jpeg
-          byte[] file_marker = new byte[2];
-          buff.read(file_marker);
+        //read the marker to make sure it is a jpeg
+        byte[] file_marker = new byte[2];
+        buff.read(file_marker);
               
-          buff.close();
+        buff.close();
               
-         //check the file type and exif validation
-         if( (file_marker[0] & 0xFF) == 0xFF && (file_marker[1] & 0xFF) == 0xD8 )
-              return true;
-         else return false;
+        //check the file type and exif validation
+        if( (file_marker[0] & 0xFF) == 0xFF && (file_marker[1] & 0xFF) == 0xD8 )
+        	return true;
+        else return false;
   	}
 
     /* protected ... ... readImageMeta(...) {
