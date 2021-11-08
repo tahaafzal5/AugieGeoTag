cba1f381abac6475bdb276558b2c0f6821222679
Sicheng Yang <43109893+SichengYang@users.noreply.github.com>
2021-11-08 16:49:01 -0600

    Update GeoTagFunctions.java
    isJpeg(File f) is done. I made it static so that you do not need to create an Object.

--- src/GeoTagFunctions.java
+++ src/GeoTagFunctions.java
@@ -1,7 +1,25 @@
+import java.io.*;
+
 public class GeoTagFunctions {
-    /* protected ... boolean isJpeg(...) {
-        ...
-    } */
+    
+    //Throw: FileNotFoundException if the file does not exist. IOException if the program cannot read the file
+  	//Return: true if passed file is a JPEG/JPG; false other wise.
+  	public static boolean isJpeg(File f) throws IOException, FileNotFoundException
+  	{    
+  		if(!f.exists()) throw new FileNotFoundException("File does not exists");
+          BufferedInputStream buff = new BufferedInputStream(new FileInputStream(f));
+              
+          //read the marker to make sure it is a jpeg
+          byte[] file_marker = new byte[2];
+          buff.read(file_marker);
+              
+          buff.close();
+              
+         //check the file type and exif validation
+         if( (file_marker[0] & 0xFF) == 0xFF && (file_marker[1] & 0xFF) == 0xD8 )
+              return true;
+         else return false;
+  	}
 
     /* protected ... ... readImageMeta(...) {
         ...
