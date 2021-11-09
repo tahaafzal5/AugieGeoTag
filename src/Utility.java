eeb1e0c0207f7994e3ad68141777c0664116f86b
Taha Afzal <tahaafzal5@hotmail.com>
2021-11-09 01:03:44 -0600

    added tests for the isJpeg method

--- src/Utility.java
+++ src/Utility.java
@@ -1,3 +1,5 @@
+package src;
+
 public class Utility {
 
     protected static void displayMenu() {
---
+++ tests/Tests.java
@@ -0,0 +1,50 @@
+package tests;
+
+import src.GeoTagFunctions;
+
+import java.util.*;
+import java.io.*;
+
+public class Tests {
+    public static void main(String[] args) {
+
+        // Testing isJpeg() method
+
+        System.out.println("Testing isJpeg method:");
+        
+        File assetsFolder = new File("./assets");
+        
+        ArrayList<String> assetsFiles = new ArrayList<>();
+        ArrayList<Boolean> expectedAnswers = new ArrayList<>();
+        
+        getAllFilesForFolder(assetsFolder, assetsFiles);
+        isJpegExpectedAnswers(expectedAnswers);
+        
+        Boolean result;
+        for (int i = 0; i < assetsFiles.size(); i++) {
+            String fileName = "./assets/" + assetsFiles.get(i);
+            result = GeoTagFunctions.isJpeg(new File(fileName));
+            
+            if (result.equals(expectedAnswers.get(i))) {
+                System.out.println("\u2713");
+            }
+            else {
+                System.out.println("Test failed \u2717");
+            }
+        }
+        System.out.println("Done testing isJpeg method");
+        System.out.println("-------------------------------------------------------------------------");
+    }
+    
+    // for the isJpeg method
+    public static void getAllFilesForFolder(File folder, ArrayList<String> fileList) {
+        for (File file : folder.listFiles()) {
+            fileList.add(file.getName());
+        }
+    }
+    
+    // for the isJpeg method
+    public static void isJpegExpectedAnswers(ArrayList<Boolean> expectedAnswers) {
+        expectedAnswers.add(true); expectedAnswers.add(true); expectedAnswers.add(true); expectedAnswers.add(true); expectedAnswers.add(true); expectedAnswers.add(false); expectedAnswers.add(true); expectedAnswers.add(true); expectedAnswers.add(true); expectedAnswers.add(true); expectedAnswers.add(true); expectedAnswers.add(false);
+    }
+}
