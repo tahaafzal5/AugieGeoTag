80f73d26880ea37f9da422a21f148807d5c02f2f
Sicheng Yang <43109893+SichengYang@users.noreply.github.com>
2021-11-09 01:58:53 -0600

    Update GeoTagFunctions.java
    Remove wrong comment and output message.

--- src/GeoTagFunctions.java
+++ src/GeoTagFunctions.java
@@ -2,19 +2,19 @@ import java.io.*;
 
 public class GeoTagFunctions {
     
-    //Throw: FileNotFoundException if the file does not exist and IOException if the program cannot read the file.
   	//Return: true if passed file is a JPEG/JPG, false otherwise.
+    //Output: Error message if the file reading process contains error.
   	public static boolean isJpeg(File file) {    
         try {
             BufferedInputStream bufferedInputStream = new BufferedInputStream(new FileInputStream(file));
 
-            //read the marker to make sure it is a jpeg
+            //read the marker
             byte[] fileMarker = new byte[2];
             bufferedInputStream.read(fileMarker);
                 
             bufferedInputStream.close();
                 
-            //check the file type and exif validation
+            //check the file type
             if ((fileMarker[0] & 0xFF) == 0xFF && (fileMarker[1] & 0xFF) == 0xD8)
                 return true;
             else
