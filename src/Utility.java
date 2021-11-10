165e82531e4494765ff1661c91424c0728ba6901
Tony Lanier <alanier18@ole.augie.edu>
2021-11-10 12:30:33 -0600

    Handling of user choice completed. Method headers for opening image and displaying info about program added.

--- src/Utility.java
+++ src/Utility.java
@@ -33,7 +33,7 @@ public class Utility {
         else if (choice == "7")
             System.exit(0);
         else {
-            System.out.println("Invalid option. Please enter option 1-7.")
+            System.out.println("Invalid option. Please enter option 1-7.");
             displayMenu();
         }
     } 
