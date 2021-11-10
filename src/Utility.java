48916ce8a5f9115c39e254d60528dac45b2519b7
Zach <zwjansen19@ole.augie.edu>
2021-11-10 12:17:01 -0600

    displayHelpMenu method complete, only worry I have is the ability to return to the original menu - ZJ

--- src/Utility.java
+++ src/Utility.java
@@ -1,5 +1,6 @@
-public class Utility {
+import java.util.Scanner;
 
+public class Utility {
     protected static void displayMenu() {
         System.out.println("Augie GeoTag Menu:");
         System.out.println("\t1. Open an image");
@@ -11,9 +12,23 @@ public class Utility {
         System.out.println("\t7. Exit");
     }
 
-    /* protected static void displayHelpMenu() {
+    protected static void displayHelpMenu() {
+        Scanner displayHelpMenuScanner = Scanner(System.in);
+        System.out.println("Help Menu:");
+        System.out.println("\t1. Open an image: This option allows you to select the specific image you would like to edit the GeoTag of. It must be the exact file name including the file tag.");
+        System.out.println("\t2. Add GeoTag: This option allows you to add a GeoTag to the image you opened in option 1. It will ask for a GeoTag in format of:  ");
+        System.out.println("\t3. Update GeoTag: This option allows you to remove an original GeoTag, and add a new one. Also known as updating. It will ask for a GeoTag in format of: ");
+        System.out.println("\t4. Remove GeoTag: This function allows you to remove the GeoTag on the image you opened in option 1.");
+        System.out.println("\t6. About: This GeoTag editing program was developed by Taha, Taire, Zach, Tony, Sicheng and Zemenu.");
+        System.out.println("Press Enter key to return to main menu.");
+        if (displayHelpMenuScanner.nextLine().equals("")) {
+            displayMenu();
+        }
+        else {
+            displayHelpMenu();
+        }
         
-    } */
+    } 
 
     /* protected ... ... handleUserMenuChoice(...) {
         ...
