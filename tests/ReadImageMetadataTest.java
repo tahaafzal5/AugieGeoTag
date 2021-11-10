2e372988d39b23b6158261faf5a35928152888d1
Taha Afzal <tahaafzal5@hotmail.com>
2021-11-10 13:11:56 -0600

    small format changes

--- tests/ReadImageMetadataTest.java
+++ tests/ReadImageMetadataTest.java
@@ -5,11 +5,11 @@ import java.io.*;
 import src.GeoTagFunctions;
 
 public class ReadImageMetadataTest {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         File assetsFolder = new File("./assets");
-        for(File jpeg : assetsFolder.listFiles())
-            if( GeoTagFunctions.readImageMeta(jpeg) )
-                System.out.printf("%-30s %c \n",jpeg.getName() , '\u2713');
+        
+        for (File jpeg : assetsFolder.listFiles())
+            if (GeoTagFunctions.readImageMeta(jpeg))
+                System.out.printf("%-30s %c \n", jpeg.getName(), '\u2713');
     }
 }
