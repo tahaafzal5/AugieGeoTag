13ecbe17f324b59b38a79d10e04be9fc959ce7c5
Sicheng Yang <43109893+SichengYang@users.noreply.github.com>
2021-11-10 08:58:23 -0600

    Add files via upload

---
+++ src/src/App.java
@@ -0,0 +1,8 @@
+package src;
+
+public class App {
+    public static void main(String[] args) {
+        Utility.displayMenu();
+        
+    }
+}
---
+++ src/src/Utility.java
@@ -0,0 +1,77 @@
+package src;
+
+public class Utility {
+
+    protected static void displayMenu() {
+        System.out.println("Augie GeoTag Menu:");
+        System.out.println("\t1. Open an image");
+        System.out.println("\t2. Add GeoTag");
+        System.out.println("\t3. Update GeoTag");
+        System.out.println("\t4. Remove GeoTag");
+        System.out.println("\t5. Help Menu");
+        System.out.println("\t6. About");
+        System.out.println("\t7. Exit");
+    }
+
+    /* protected static void displayHelpMenu() {
+        
+    } */
+
+    /* protected ... ... handleUserMenuChoice(...) {
+        ...
+    } */
+
+    /* protected ... void askConfirmation(...) {
+
+    } */
+
+    /* protected ... ... handleUserConfirmationChoice(...) {
+        ...
+    } */
+
+    /* protected ... void displayError(...) {
+        ...
+    } */
+
+    public static void displaySuccess(String type) {
+        switch (type) {
+            case "open-image":
+                System.out.println("Image opened successfully"); break;
+            case "read-metadata":
+                System.out.println("Metadata read successfully"); break;
+            case "check-jpeg":
+                System.out.println("Checked JPEG/JPG successfully"); break;
+            case "read-geotag":
+                System.out.println("Read geotag successfully"); break;
+            case "remove-geotag":
+                System.out.println("Removed geotag successfully"); break;
+            case "write-geotag":
+                System.out.println("Wrote geotag successfully"); break;
+            case "save-image":
+                System.out.println("Image saved successfully"); break;
+            default:
+                System.out.println("Success");
+        }
+    } 
+
+    public static void displayProcessing(String type) {
+        switch (type) {
+            case "open-image":
+                System.out.println("Opening image..."); break;
+            case "read-metadata":
+                System.out.println("Reading image metadata..."); break;
+            case "check-jpeg":
+                System.out.println("Checking if the image is a JPEG/JPG..."); break;
+            case "read-geotag":
+                System.out.println("Reading the geotag in the image..."); break;
+            case "remove-geotag":
+                System.out.println("Removing geotag from the image..."); break;
+            case "write-metadata":
+                System.out.println("Writing the metadata in the image..."); break;
+            case "save-image":
+                System.out.println("Saving the image..."); break;
+            default:
+                System.out.println("Processing...");
+        }
+    }
+}
