1061a20f8735fe790594714ae0fc942813438bb9
Sicheng Yang <43109893+SichengYang@users.noreply.github.com>
2021-11-10 12:41:53 -0600

    Add files via upload
    test file

---
+++ tests/ReadImageMetadataTest.java
@@ -0,0 +1,15 @@
+package tests;
+
+import java.io.*;
+
+import src.GeoTagFunctions;
+
+public class ReadImageMetadataTest {
+    public static void main(String[] args)
+    {
+        File assetsFolder = new File("./assets");
+        for(File jpeg : assetsFolder.listFiles())
+            if( GeoTagFunctions.readImageMeta(jpeg) )
+                System.out.printf("%-30s %c \n",jpeg.getName() , '\u2713');
+    }
+}
