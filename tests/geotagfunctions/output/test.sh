a1ad26196ca209d8a3c102052bc11a6e2944b2c3
杨思成 <43109893+SichengYang@users.noreply.github.com>
2021-12-01 11:10:18 -0600

    output test will be added later

--- tests/geotagfunctions/output/test.sh
+++
@@ -1,7 +0,0 @@
-echo "-------------"
-echo "Update geotag test:"
-java -cp .:./lib/commons-imaging-1.0-alpha2/commons-imaging-1.0-alpha2.jar:./lib/exif.jar tests/geotagfunctions/output/UpdateGeoTagTest
-echo "-------------"
-echo "Remove geotag test:"
-java -cp .:./lib/commons-imaging-1.0-alpha2/commons-imaging-1.0-alpha2.jar:./lib/exif.jar tests/geotagfunctions/output/RemoveGeoTagTest
-echo "-------------"
