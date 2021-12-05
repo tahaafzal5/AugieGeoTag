71345242632cd64e652f78cfdb8efea7446d2669
杨思成 <43109893+SichengYang@users.noreply.github.com>
2021-12-05 00:32:43 -0600

    update utility test using JUnit

--- tests/utility/output/test.sh
+++ tests/utility/output/test.sh
@@ -1,4 +1,2 @@
 java -cp .:./lib/hamcrest-core-1.3.jar:./lib/junit-4.13.2.jar tests.utility.output.UtilityOutputTestRunner
-echo "difference between test and answer (empty means no difference):"
-diff ./tests/utility/output/answer.txt ./tests/utility/output/print.txt
 echo "-------------"
