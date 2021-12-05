84214f130212e2628a3622ec734423521840148c
杨思成 <43109893+SichengYang@users.noreply.github.com>
2021-12-05 00:32:43 -0600

    update utility test using JUnit

--- tests/utility/output/UtilityOutputTestRunner.java
+++ tests/utility/output/UtilityOutputTestRunner.java
@@ -1,15 +1,24 @@
 package tests.utility.output;
 
-import java.io.*;
 import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+import java.io.*;
 
 public class UtilityOutputTestRunner {
 
 	public static void main(String[] args) throws FileNotFoundException
 	{
+		PrintStream console = System.out;
 		PrintStream fileOutput = new PrintStream(new File("./tests/utility/output/print.txt"));
 		System.setOut(fileOutput);
 		System.setErr(fileOutput);
-		JUnitCore.runClasses(UtilityOutputTest.class);
+		Result result = JUnitCore.runClasses(UtilityOutputTest.class);
+		
+		for (Failure failure : result.getFailures())
+		    console.println(failure.toString());
+		
+		console.println("Utility output test result: " + result.wasSuccessful());
+		console.close();
 	}
 }
